26323bb5a0f6e789b079c5bd53ba02d3d2a66443
Shari Ebach <ebachshari@yahoo.com>
2024-04-17 18:09:40 -0500

    added css

--- components/AllItemsCard.js
+++ components/AllItemsCard.js
@@ -18,10 +18,10 @@ function AllItemsCard({ itemObj, orderId, onUpdate }) {
       <Card className="itemCard" style={{ width: '16rem', margin: '25px' }}>
         <Card.Img variant="top" style={{ width: '14rem', margin: '10px' }} src={itemObj.image} alt={itemObj.name} />
         <h4 className="card-text bold">{itemObj.name}</h4>
-        <h5 className="card-text bold">{itemObj.orderPrice}</h5>
+        <h5 className="card-text bold">${itemObj.orderPrice}</h5>
         {/* <Card.Img variant="top" src={ItemObj.image} alt={ItemObj.name} /> */}
         <div>
-          <Button variant="outline-success" size="sm" onClick={handleAddItemClick} className="deleteBtn m-2">
+          <Button variant="outline-dark" size="sm" onClick={handleAddItemClick} className="deleteBtn m-2">
             Add Item
           </Button>
         </div>
